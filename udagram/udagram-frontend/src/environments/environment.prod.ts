05b11619f65cac30b6e91c4b9ccc5964a43fe923
Ahmed Oraby <86479380+Ahmed-Oraby@users.noreply.github.com>
2022-11-04 14:03:27 +0200

    Fixing some issues.

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+	production: true,
+	appName: 'Udagram',
+	apiHost: 'http://udagram-backend-dev.eba-gg3cktmn.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
